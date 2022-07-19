7c23447b708ba52062d01792feeb137f517c97c6
JD Smith <93749+jdtsmith@users.noreply.github.com>
2022-07-19 08:24:03 -0400

    Factor out fixed/missing tests for bounded columns/vars

--- pahfit/features/features_format.py
+++ pahfit/features/features_format.py
@@ -1,9 +1,12 @@
+"""pahfit.features.feature_forrmat - Special handling for bounded (val,
+    min, max) values, which are represented as 3 element masked arrays
+    in pahfit.
+"""
 import numpy.ma as ma
 from astropy.table import MaskedColumn
 from astropy.table.pprint import TableFormatter
+from pahfit.util import bounded_is_fixed, bounded_is_missing
 
-
-# * Special table formatting for bounded (val, min, max) values
 def fmt_func(fmt):
     def _fmt(v):
         if ma.is_masked(v[0]):
@@ -13,10 +16,9 @@ def fmt_func(fmt):
         return f'{v[0]:{fmt}} ({v[1]:{fmt}}, {v[2]:{fmt}})'
     return _fmt
 
-
 class BoundedMaskedColumn(MaskedColumn):
-    """Masked column which can be toggled to group rows into one item
-    for formatting.  To be set as Table's `MaskedColumn'.
+    """Masked column which groups rows into a single item for formatting.
+    To be set as Table's `MaskedColumn'.
     """
     _omit_shape = False
 
@@ -25,16 +27,24 @@ class BoundedMaskedColumn(MaskedColumn):
         sh = super().shape
         return sh[0:-1] if self._omit_shape and len(sh) > 1 else sh
 
-    def is_fixed(self):
-        return ma.getmask(self)[:,1:].all(1)
-
+    def is_missing(self):
+        """Return a boolean array indicating which elements of the
+        column are missing (i.e. their value is masked).
+        """
+        return bounded_is_missing(self)
         
+    def is_fixed(self):
+        """Return a boolean array indicating which elements of the
+        column are fixed (i.e. they have both bounds masked).
+        """
+        return bounded_is_fixed(self)
+    
 class BoundedParTableFormatter(TableFormatter):
-    """Format bounded parameters.
-    Bounded parameters are 3-field structured arrays, with fields
-    'var', 'min', and 'max'.  To be set as Table's `TableFormatter'.
+    """Format bounded parameters.  Bounded parameters are 3-field
+    arrays, corresponding to 'value', 'min', and 'max'.  To be set as
+    Table's `TableFormatter'.
     """
-
+    
     def _pformat_table(self, table, *args, **kwargs):
         bpcols = []
         try:
